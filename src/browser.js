30139ccf07b4addc602c69e394eb053aac13ec6f
Leonardo Quixadá <lquixada@gmail.com>
2017-10-30 10:18:56 -0200

    cleaner approach for Promise based browser cookies.

--- src/browser.js
+++ src/browser.js
@@ -1,61 +1,61 @@
 import { stringify, isObject } from './helpers';
 
-const set = (key, val) =>
-  new Promise(resolve => {
-    const obj = isObject(key)? key : {[key]: val};
+const set = (key, val) => {
+  const obj = isObject(key)? key : {[key]: val};
 
-    Object.keys(obj)
-      .forEach(key => {
-        document.cookie = `${encodeURIComponent(key)}=${stringify(obj[key])}`;
-      });
+  Object.keys(obj)
+    .forEach(key => {
+      document.cookie = `${encodeURIComponent(key)}=${stringify(obj[key])}`;
+    });
 
-    resolve();
-  });
+  return Promise.resolve();
+};
 
-const get = key =>
-  new Promise(resolve => {
-    key = encodeURIComponent(key);
-    const match = document.cookie.match(new RegExp(`(${key})=(.*?)(;|$)`));
-    resolve(match ? match[2] : undefined);
-  });
+const get = key => {
+  key = encodeURIComponent(key);
+  const match = document.cookie.match(new RegExp(`(${key})=(.*?)(;|$)`));
 
-const getAll = () =>
-  new Promise(resolve => {
-    const cookies = {};
-    document.cookie
-      .replace(/; /g, ';')
-      .split(';')
-      .map(cookie => cookie.split('='))
-      // Filters only those who has a non-empty key (happens when you delete one)
-      .filter(([key, ]) => key)
-      .forEach(([key, val]) => cookies[encodeURIComponent(key)] = val);
-    resolve(cookies);
-  });
+  return Promise.resolve(match ? match[2] : undefined);
+};
 
-const remove = key =>
-  new Promise(resolve => {
-    key = encodeURIComponent(key);
-    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
-    resolve();
-  });
+const getAll = () => {
+  const cookies = {};
 
-const clearAll = () =>
-  new Promise(resolve => {
-    document.cookie
-      .replace(/; /g, ';')
-      .split(';')
-      .map(cookie => cookie.split('=')[0])
-      .forEach(key => {
-        remove(key);
-      });
-    resolve();
-  });
+  document.cookie
+    .replace(/; /g, ';')
+    .split(';')
+    .map(cookie => cookie.split('='))
+    // Filters only those who has a non-empty key (happens when you delete one)
+    .filter(([key, ]) => key)
+    .forEach(([key, val]) => cookies[encodeURIComponent(key)] = val);
+
+  return Promise.resolve(cookies);
+};
+
+const remove = key => {
+  key = encodeURIComponent(key);
+  document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+
+  return Promise.resolve();
+};
+
+const clearAll = () => {
+  document.cookie
+    .replace(/; /g, ';')
+    .split(';')
+    .map(cookie => cookie.split('=')[0])
+    .forEach(remove);
+
+  return Promise.resolve();
+};
 
 // Used only for tests, it's private, don't use it
 const _mock = obj => {
-  Object.keys(obj).forEach(key => {
-    document.cookie = `${key}=${stringify(obj[key])}`;
-  });
+  Object.keys(obj)
+    .forEach(key => {
+      document.cookie = `${key}=${stringify(obj[key])}`;
+    });
+
   return Promise.resolve();
 };
 
